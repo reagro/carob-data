526500c8592f2bb05dcff900a1890aee1db2787d
rhijmans <r.hijmans@gmail.com>
2024-06-29 11:40:49 -0700

    r

--- _script/copy_reports.R
+++ _script/copy_reports.R
@@ -4,6 +4,11 @@ inf <- list.files("../carob/data/clean/", pattern="html$", full=TRUE, recursive=
 outf <- gsub("../carob/data/clean/", "build/html/reports/", inf)
 folders <- paste0(unique(dirname(outf)))
 s <- sapply(folders, \(f) dir.create(f, FALSE, TRUE))
-#s <- file.copy(inf, outf, overwrite=FALSE)
 s <- file.copy(inf, outf, overwrite=TRUE, copy.date=TRUE)
-print(table(s))
+#print(table(s))
+
+
+inf <- list.files("../carob/data/compiled/", pattern="html$", full=TRUE, recursive=FALSE)
+outf <- gsub("../carob/data/compiled/", "build/html/reports/", inf)
+s <- file.copy(inf, outf, overwrite=TRUE, copy.date=TRUE)
+#print(table(s))
